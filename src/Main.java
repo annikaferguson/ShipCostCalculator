52d54fae169a1b6dfdfa41d5184fd221d7770d32
annikaferguson <fergusak@mail.uc.edu>
2023-06-08 11:20:21 -0400

    final with double input bug

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        double price = 0;
+        double priceShipping = 0;
+        double totalPrice = 0;
+        String trash = ""; //bad input
+        String prompt = "Enter the price of the item: ";
+
+        System.out.println("Enter the price of the item: ");
+        price = in.nextDouble();
+
+        if(in.nextDouble() >= 100)
+        {
+            System.out.println("Shipping is free! Your total is " + price);
+        }
+        else if (price < 100)
+        {
+            System.out.println("Shipping is 2% of the item price.");
+            priceShipping = price * 0.02;
+            System.out.println("Shipping will cost " + priceShipping);
+            totalPrice = price + priceShipping;
+            System.out.println("Your total cost is " + totalPrice);
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("\nYou said price was: " + trash);
+            System.out.println("Run the program again and enter a valid price.");
+        }
     }
 }
